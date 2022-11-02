97f7bb5386db3b690306697dd7ce6200c8c960a0
mansi rajwar <85187579+mansirajwar13@users.noreply.github.com>
2022-11-02 07:50:25 +0000

    jobs doing

--- app.py
+++ app.py
@@ -2,9 +2,45 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+JOBS =[
+  {
+    'id':1,
+    'title':'data analyst',
+    'location':'bengaluru India',
+    'salary':'Rs. 1,00,000'
+    
+  },
+  {
+  'id':2,
+    'title':'data scientist',
+    'location':'bengaluru India',
+    'salary':'Rs. 15,00,000'
+    
+  },
+  {
+  'id':3,
+    'title':'frontend engineer',
+    'location':'remote',
+    'salary':'Rs. 12,00,000'
+  },
+  {
+    {
+  'id':4,
+    'title':'backend engineer',
+    'location':'san francisco,USA',
+    'salary':'$12,00,000'
+    } 
+  }
+  
+  
+  
+  
+  
+  
+]
 @app.route("/")
 def hello_world():
-  return render_template('home.html')
+  return render_template('home.html',jobs=JOBS)
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0',debug=True)
