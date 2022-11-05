48220dcee4db04447cc5ed82b598ce437dc5dd06
mansi rajwar <85187579+mansirajwar13@users.noreply.github.com>
2022-11-05 03:13:59 +0000

    Added requirements.txt

--- app.py
+++ app.py
@@ -1,44 +1,42 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
 
-JOBS =[
+JOBS = [
   {
-    'id':1,
-    'title':'data analyst',
-    'location':'bengaluru India',
-    'salary':'Rs. 1,00,000'
-    
+    'id': 1,
+    'title': 'Data Analyst',
+    'location': 'Bengaluru, India',
+    'salary': 'Rs. 10,00,000'
   },
   {
-  'id':2,
-    'title':'data scientist',
-    'location':'bengaluru India',
-    'salary':'Rs. 15,00,000'
-    
+    'id': 2,
+    'title': 'Data Scientist',
+    'location': 'Delhi, India',
+    'salary': 'Rs. 15,00,000'
   },
   {
-  'id':3,
-    'title':'frontend engineer',
-    'location':'remote',
-    'salary':'Rs. 12,00,000'
+    'id': 3,
+    'title': 'Frontend Engineer',
+    'location': 'Remote'
   },
   {
-    'id':4,
-    'title':'backend engineer',
-    'location':'san francisco,USA',
-    'salary':'$12,00,000' 
+    'id': 4,
+    'title': 'Backend Engineer',
+    'location': 'San Francisco, USA',
+    'salary': '$150,000'
   }
-  
-  
-  
-  
-  
-  
 ]
+
 @app.route("/")
-def hello_world():
-  return render_template('home.html',jobs=JOBS,company_name='jovian')
+def hello_jovian():
+    return render_template('home.html', 
+                           jobs=JOBS, 
+                           company_name='Jovian')
+
+@app.route("/api/jobs")
+def list_jobs():
+  return jsonify(JOBS)
 
 if __name__ == '__main__':
-  app.run(host='0.0.0.0',debug=True)
+  app.run(host='0.0.0.0', debug=True)
